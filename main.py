9c1d479c8b1768d98d10e04ff5a0be08cea22c14
Rishabh Anand <geniusrishabhanand@gmail.com>
2018-03-20 23:10:42 +0530

    updation of face + GSM + Voice +RFID

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ import facedetect
 import os
  
 ArduinoSerial1 = serial.Serial('com4',9600)
-#ArduinoSerial2 = serial.Serial('com13',9600)
+ArduinoSerial2 = serial.Serial('com3',9600)
 time.sleep(2)
 t = 9897550614153
 r = 26390579686429
 s = 73669285398244
 u = 73669285449134
 while 1:
+        ArduinoSerial1.write('0')
         os.system('cls')
         print ("Enter your choice")
         print ("1. For RFID card detection")
@@ -29,9 +30,11 @@ while 1:
                 if y == t:
                         print "RFID found : Welcome Rishabh, Your ID is",y
                         ArduinoSerial1.write('1')
+                        ArduinoSerial2.write('z')
                         print "Gate Unlocked"
                         time.sleep(1)
                         ArduinoSerial1.write('0')
+                        ArduinoSerial2.write('0')
                         db = MySQLdb.connect(host="localhost",user="aecs1",passwd="aecs1",db="home")
                         cur = db.cursor()
                         cur.execute("INSERT INTO access(RFID,NAME,EMAIL) VALUES('9897550614153','Rishabh','geniusrishabhanand@gmail.com')")
@@ -47,9 +50,11 @@ while 1:
                 elif y == r:
                         print "RFID found :Welcome Saurabh, Your ID is",y
                         ArduinoSerial1.write('1')
+                        ArduinoSerial2.write('z')
                         print "Gate Unlocked"
                         time.sleep(1)
                         ArduinoSerial1.write('0')
+                        ArduinoSerial2.write('0')
                         db = MySQLdb.connect(host="localhost",user="aecs1",passwd="aecs1",db="home")
                         cur = db.cursor()
                         cur.execute("INSERT INTO access(RFID,NAME,EMAIL) VALUES('26390579686429','Saurabh','saurabh2206@gmail.com')")
@@ -58,13 +63,14 @@ while 1:
                         server = smtplib.SMTP('smtp.gmail.com',587)
                         server.starttls()
                         server.login("radheysen0@gmail.com","9413261316")
-                        server.sendmail("radheysen0@gmail.com","geniusrishabhanand@gmail.com","Hi Admin, Saurabh has entered your house")
-                        server.sendmail("radheysen0@gmail.com","saurabh2206@gmail.com","Hi Admin, Saurabh has entered your house")
+                        server.sendmail("radheysen0@gmail.com","geniusrishabhanand@gmail.com","Hi Admin, Sangamitra has entered your house")
+                        server.sendmail("radheysen0@gmail.com","saurabh2206@gmail.com","Hi Admin, Sangamitra has entered your house")
                         server.quit()
                 elif y == s:
                         #print "found"
                         print "RFID found :Welcome Sangamitra, Your ID is",y
                         ArduinoSerial1.write('1')
+                        ArduinoSerial2.write('z')
                         print "Gate Unlocked"
                         time.sleep(1)
                         db = MySQLdb.connect(host="localhost",user="aecs1",passwd="aecs1",db="home")
@@ -73,17 +79,19 @@ while 1:
                         db.commit()
                         db.close()
                         ArduinoSerial1.write('0')
+                        ArduinoSerial2.write('0')
                         server = smtplib.SMTP('smtp.gmail.com',587)
                         server.starttls()
                         server.login("radheysen0@gmail.com","9413261316")
-                        server.sendmail("radheysen0@gmail.com","geniusrishabhanand@gmail.com","Hi Admin, Sangamitra has entered your house")
-                        server.sendmail("radheysen0@gmail.com","saurabh2206@gmail.com","Hi Admin, Sangamitra has entered your house")
+                        server.sendmail("radheysen0@gmail.com","geniusrishabhanand@gmail.com","Hi Admin, Saurabh has entered your house")
+                        server.sendmail("radheysen0@gmail.com","saurabh2206@gmail.com","Hi Admin, Saurabh has entered your house")
                         server.quit()
                 elif y == u:
                         print "Sorry,Gate locked"
                         winsound.Beep(1000,2000)
                         time.sleep(1)
                         ArduinoSerial1.write('0')
+                        ArduinoSerial2.write('0')
                         db = MySQLdb.connect(host="localhost",user="aecs1",passwd="aecs1",db="home")
                         cur = db.cursor()
                         cur.execute("INSERT INTO access(RFID,NAME,EMAIL) VALUES('73669285449134','Unknown','Unknown')")
@@ -117,22 +125,26 @@ while 1:
                                 print "Your Face is recognised!!!!"
                                 print "Welcome Home!!!"
                                 ArduinoSerial1.write('1')
+                                ArduinoSerial2.write('z')
                                 print "Gate Unlocked"
                                 time.sleep(1)
                                 db = MySQLdb.connect(host="localhost",user="aecs1",passwd="aecs1",db="home")
                                 cur = db.cursor()
-                                cur.execute("INSERT INTO access(RFID,NAME,EMAIL) VALUES('0000000','Admin User','saurabh2206@gmail.com')")
+                                cur.execute("INSERT INTO access(RFID,NAME,EMAIL) VALUES('0000000','Face User','saurabh2206@gmail.com')")
                                 db.commit()
                                 db.close()
                                 ArduinoSerial1.write('0')
+                                ArduinoSerial2.write('0')
                                 server = smtplib.SMTP('smtp.gmail.com',587)
                                 server.starttls()
                                 server.login("radheysen0@gmail.com","9413261316")
                                 server.sendmail("radheysen0@gmail.com","geniusrishabhanand@gmail.com","Hi Admin, You have accessed your gate")
-                                server.sendmail("radheysen0@gmail.com","saurabh2206@gmail.com","Hi Admin, Hi Admin, You have accessed your gate")
+                                server.sendmail("radheysen0@gmail.com","saurabh2206@gmail.com","Hi Face User, You have accessed your gate")
                                 server.quit()
                         elif a==0:
                                 print 'Face is not recognized'
+                                ArduinoSerial1.write('0')
+                                ArduinoSerial2.write('0')
 
                 except sr.UnknownValueError:
                     print("Could not understand audio")
